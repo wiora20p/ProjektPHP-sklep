9929deaa7cdf09dc122d330db39351ded5d21a0f
wiora20p <91263424+wiora20p@users.noreply.github.com>
2022-05-19 12:02:54 +0200

    strona główna css

---
+++ sklep.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="fixed-top">
+        
+        <nav class="menu-gorne">
+                <a href="sklep.php"><img src="Logo.png" alt="logo" height="40px"></a>  <!--  class="list-group-item py-1" -->
+                <input type="text" placeholder="Co szukasz?">
+                <a href="logowanie.php">logowanie</a>  <!--  class="list-group-item" -->
+        </nav>
+    </header>
+    <main>
+        <aside>
+            <ol>
+                <li>ubrania</li>
+                <li>zabawki</li>
+                <li>książki</li>
+                <li>AGD</li>
+                <li>artykuły biurowe</li>
+                <li>sprzęt domowy</li>
+            </ol>
+        </aside>
+    </main>
+</body>
+</html>
+<!-- 
+    foreach($file as $lina)
+        strpos($file, $linia) - znajduje frazę w linii (moża w ten sposób szukać loginów - )
+        str_contains($linia,$słowo) - czy jest w linii? (może weryfikować hasła)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        trello-tabele(projekty)
+  -->
