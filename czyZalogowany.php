1f568deb66dc8f6d5d4947ce5ab70d0f35a0c46a
wiora20p <91263424+wiora20p@users.noreply.github.com>
2022-05-20 13:07:35 +0200

    Add files via upload

---
+++ czyZalogowany.php
@@ -0,0 +1,4 @@
+<?php
+if(!isset($_SESSION ['login'])){
+    header("Location:a.html");
+}
--- sklep.php
+++ sklep.php
@@ -1,19 +1,29 @@
+<?php
+ob_start();
+//include 'czyZalogowany.php';
+$produkty = file("produkty.txt");
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sklep</title>
+    <title>Strona główna</title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header class="fixed-top">
-        
         <nav class="menu-gorne">
                 <a href="sklep.php"><img src="Logo.png" alt="logo" height="40px"></a>  <!--  class="list-group-item py-1" -->
-                <div class="szukaj"><input type="text" class="wyszukiwarka" placeholder="Co szukasz?"><img src="icons8-search-24.png" alt="szukaj"></div>
+                <form class="szukaj" action="sklep.php" method="POST" >
+                    <input name="wyszukiwarka" type="text" class="wyszukiwarka" placeholder="Co szukasz?">
+                    <input type="submit" value="szukaj">
+                    <!-- <label for="">
+                        <img src="icons8-search-24.png" alt="szukaj">
+                    </label> -->
+                </form>
                 <a href="logowanie.php">logowanie</a>  <!--  class="list-group-item" -->
         </nav>
     </header>
@@ -37,26 +47,44 @@
                 <li>Sport</li>
             </ol>
         </aside>
+    <article>
+        <div class="obraz"></div>
+        <h3><?php ?>
+        </h3>
+    </article>
     </main>
+    <footer>afs</footer>
+
+<?php
+    $wyszukiwarka = $_POST['wyszukiwarka'];
+    if (!empty($wyszukiwarka)) {
+        echo "<ul>\n";
+        $dane = file("dane.txt");
+        // for ($index=0; $index < count($dane); $index++) {
+        //     $g = split(" | ", chop($dane[$index]));
+        //     $eregi = eregi($wyszukiwarka, $dane);
+
+        //     if (@eregi($wyszukiwarka, $dane[$index])) {
+        //         echo '<li><a href="'.$g[0].'" title="'.$g[1].'">'.$g[1]."</a></li>\n";
+        //         $bl = true;
+        //     }
+        // }
+
+        echo "</ul>";
+    }
+
+    //w przypadku braku wyników
+    if (!$bl) {
+        echo 'Brak wyników';
+    }
+?>
+
 </body>
 </html>
 <!-- 
     foreach($file as $lina)
         strpos($file, $linia) - znajduje frazę w linii (moża w ten sposób szukać loginów - )
         str_contains($linia,$słowo) - czy jest w linii? (może weryfikować hasła)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
